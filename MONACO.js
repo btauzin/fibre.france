accfc51e23a97eab9be80398066ec7062a0af28c
btauzin <75615091+btauzin@users.noreply.github.com>
2023-10-23 23:58:50 +0200

    Add files via upload

---
+++ MONACO.js
@@ -0,0 +1,3 @@
+var MONACO = {"type":"FeatureCollection", "features": [
+{"type":"Feature","geometry":{"type":"LineString","coordinates":[[7.41641740049513,43.73534029420677],[7.438156988015674,43.72362750266976],[7.613085357500442,43.57370530621199],[8.124310934092877,43.70359962847184],[8.286752920144798,43.94851322300573],[8.443104742357763,43.97865504298797],[8.592312561275715,44.13948037361499],[8.467245740558136,44.29836330858095]]},"properties":{"name":"Cable Monaco-Savone","styleUrl":"#m_ylw-pushpin","stroke-opacity":1,"stroke":"#ffff00","stroke-width":2.5,"icon-scale":1.1,"icon-offset":[20,2],"icon-offset-units":["pixels","pixels"],"icon":"http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png"}}
+]}
