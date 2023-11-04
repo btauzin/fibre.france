61526eeb27230f15094c9c92623d57cf7ba01da0
btauzin <75615091+btauzin@users.noreply.github.com>
2023-11-04 21:40:50 +0100

    Add files via upload

---
+++ maps/markers.js
@@ -0,0 +1,153 @@
+var markers = [
+   // BRGM
+   {
+     "name": "SITcomOptics",
+     "url": "#BRGM",
+     "lat": 47.926509,
+     "lng": 1.906364
+   },
+   // BREST
+   {
+    "name": "FOCUS",
+    "url": "#BREST",
+    "lat": 37.499737174000074,
+    "lng": 15.383913237000058
+   },
+   {
+    "name": "GUADELOUPE",
+    "url": "#BREST",
+    "lat": 16.038297,
+    "lng": -61.565259
+   },
+   // LYON
+   {
+     "name": "A40 APRR",
+     "url": "#LYON",
+     "lat": 46.133302,
+     "lng": 5.542962
+   },
+   {
+    "name": "DASARA 1",
+    "url": "#LYON",
+    "lat": 45.805556,
+    "lng": 4.771162
+   },
+   {
+    "name": "DASARA 2",
+    "url": "#LYON",
+    "lat": 45.757173,
+    "lng": 4.849587
+   },
+    // NICE
+   {
+      "name": "Le Teil",
+      "url": "#NICE",
+      "lat": 44.56149088131826,
+      "lng": 4.598153204620957
+   },
+   {
+      "name": "LSPM",
+      "url": "#NICE",
+      "lat": 43.0769869255,
+      "lng": 5.89425748639
+   },
+   {
+      "name": "MNCA",
+      "url": "#NICE",
+      "lat": 43.7023977154677,
+      "lng": 7.28094142989036
+   },
+   {
+      "name": "HCMR",
+      "url": "#NICE",
+      "lat": 36.817277,
+      "lng": 21.70715
+   },
+   {
+      "name": "NESTOR",
+      "url": "#NICE",
+      "lat": 36.81696498127454,
+      "lng": 21.70747259099767
+   },
+   {
+      "name": "MONACO",
+      "url": "#NICE",
+      "lat": 43.73534029420677,
+      "lng": 7.41641740049513
+   },
+   {
+      "name": "ABYSS Norte & Sur",
+      "url": "#NICE",
+      "lat": -29.88803932700965,
+      "lng": -71.24961473381573
+   },
+   {
+      "name": "ABYSS CONCON",
+      "url": "#NICE",
+      "lat": -32.932623,
+      "lng": -71.488037
+   },
+   // GRENOBLE
+   {
+      "name": "Grenoble",
+      "url": "#GRENOBLE",
+      "lat": 45.19752,
+      "lng": 5.772133
+   },
+   {
+      "name": "Lacs Robert",
+      "url": "#GRENOBLE",
+      "lat": 45.130651,
+      "lng": 5.916604
+   },
+   {
+      "name": "Saint-François Longchamp",
+      "url": "#GRENOBLE",
+      "lat": 45.408966,
+      "lng": 6.348209
+   },
+   {
+      "name": "Glacier de Taconnaz",
+      "url": "#GRENOBLE",
+      "lat": 45.852119,
+      "lng": 6.837616
+   },
+   {
+      "name": "Glacier d'Argentière'",
+      "url": "#GRENOBLE",
+      "lat": 45.950158,
+      "lng": 6.991231
+   },
+   {
+      "name": "Glacier de Tête Rousse'",
+      "url": "#GRENOBLE",
+      "lat": 45.856446,
+      "lng": 6.820439
+   },
+   {
+      "name": "ARGODAS'",
+      "url": "#GRENOBLE",
+      "lat": 38.163642,
+      "lng": 20.504466
+   },
+   {
+      "name": "Champs de captage de Crépieux Charmy'",
+      "url": "#GRENOBLE",
+      "lat": 45.8007867998862,
+      "lng": 4.89680819872097
+   },
+   // IPGP
+   {
+    "name": "Stromboli",
+    "url": "#IPGP",
+    "lat": 38.799319,
+    "lng": 15.230425
+   },
+   // STRASBOURG
+   {
+    "name": "StrasDAS",
+    "url": "#STRASBOURG",
+    "lat": 48.58020219318517,
+    "lng": 7.763025672959176
+   }
+];
---
+++ maps/path-fiber.js
@@ -0,0 +1,31 @@
+function lineStyle(feature) {
+    return {
+        color: "red",  // Change the color to your desired color
+        weight: 4,       // Change the line width (weight) to your desired value
+    };
+    }
+
+// Overlay the fiber routes
+var STRASBOURG = L.geoJSON(STRASBOURG, { style: lineStyle, }).addTo(map);
+
+var DASARA_12 = L.geoJSON(DASARA_12, { style: lineStyle, }).addTo(map)
+var APRR = L.geoJSON(APRR, { style: lineStyle, }).addTo(map)
+
+var BRGM = L.geoJSON(BRGM, { style: lineStyle, }).addTo(map)
+
+var FOCUS = L.geoJSON(FOCUS, { style: lineStyle, }).addTo(map)
+var BREST1 = L.geoJSON(BREST1, { style: lineStyle, }).addTo(map)   
+
+var LETEIL = L.geoJSON(LETEIL, { style: lineStyle, }).addTo(map)   
+var MEUST = L.geoJSON(MEUST, { style: lineStyle, }).addTo(map)   
+var MNCA = L.geoJSON(MNCA, { style: lineStyle, }).addTo(map)
+var HCMR = L.geoJSON(HCMR, { style: lineStyle, }).addTo(map)   
+var NESTOR = L.geoJSON(NESTOR, { style: lineStyle, }).addTo(map)
+var MONACO = L.geoJSON(MONACO, { style: lineStyle, }).addTo(map)   
+var ABYSS = L.geoJSON(ABYSS, { style: lineStyle, }).addTo(map)   
+
+var GREFIB = L.geoJSON(GREFIB, { style: lineStyle, }).addTo(map)   
+var GRECHA = L.geoJSON(GRECHA, { style: lineStyle, }).addTo(map)   
+var ARGODAS = L.geoJSON(ARGODAS, { style: lineStyle, }).addTo(map)
+
+var IPGP = L.geoJSON(IPGP, { style: lineStyle, }).addTo(map)
